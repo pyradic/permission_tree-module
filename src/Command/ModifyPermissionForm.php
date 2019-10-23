a81dff2d60894f29c94b2c96455c6c8db3cd4c5d
Robin Radic <rradic@hotmail.com>
2019-10-23 16:44:12 +0200

    Platform service providers, permission sets and permission updates

--- src/Command/ModifyPermissionForm.php
+++ src/Command/ModifyPermissionForm.php
@@ -2,6 +2,7 @@
 
 namespace Pyro\PermissionTreeModule\Command;
 
+use Anomaly\Streams\Platform\Addon\AddonCollection;
 use Anomaly\Streams\Platform\Asset\Asset;
 use Illuminate\Contracts\Container\Container;
 use Illuminate\Support\Arr;
@@ -24,31 +25,44 @@ class ModifyPermissionForm
      * @return void
      * @throws \Exception
      */
-    public function handle(Container $container, Asset $assets)
+    public function handle(Container $container, Asset $assets, AddonCollection $addons)
     {
         $this->builder->setFormOption('class', 'pyro-permission-tree__permission-form');
         $this->builder->setFormOption('form_view', 'pyro.module.permission_tree::permission-form');
 
-        $data = collect($this->builder->getSections())->map(function ($section, $key) {
-            $addon[ 'key' ]     = $key;
-            $addon[ 'label' ]   = trans($section[ 'title' ]);
-            $addon[ 'streams' ] = collect($section[ 'fields' ])->map(function ($fieldSlug) {
-                $field       = $this->builder->getFormField($fieldSlug);
-                $value       = Arr::wrap($field->getValue());
-                $fieldKey    = str_replace('_', '.', $fieldSlug);
-                $permissions = [];
-                foreach ($field->config('options') as $key => $label) {
-                    $label   = trans($label);
-                    $enabled = in_array($key, $value, true);
-
-                    $permission    = Str::replaceFirst($fieldKey . '.', '', $key);
-                    $permissions[] = compact('label', 'enabled', 'key', 'permission');
+        $data = collect($this->builder->getSections())
+            ->filter(function ($section, $key) use ($addons) {
+                if ($addons->has($key)) {
+                    /** @var \Anomaly\Streams\Platform\Addon\Module\Module|\Anomaly\Streams\Platform\Addon\Extension\Extension $addon */
+                    $addon = $addons->get($key);
+                    if ($addon->isInstalled() === false) {
+                        return false;
+                    }
                 }
+                return true;
+            })
+            ->map(function ($section, $key) {
+                $addon[ 'key' ] = $key;
+                $addon[ 'label' ]   = trans($section[ 'title' ]);
+                $addon[ 'streams' ] = collect($section[ 'fields' ])->map(function ($fieldSlug) {
+                    $field       = $this->builder->getFormField($fieldSlug);
+                    $value       = Arr::wrap($field->getValue());
+                    $fieldKey    = str_replace('_', '.', $fieldSlug);
+                    $permissions = [];
+                    foreach ($field->config('options') as $key => $label) {
+                        $label   = trans($label);
+                        $enabled = in_array($key, $value, true);
 
-                return [ 'permissions' => $permissions, 'label' => $field->getLabel(), 'field' => $fieldSlug, 'key' => $fieldKey ];
-            })->toArray();
-            return $addon;
-        })->values()->toArray();
+                        $permission    = Str::replaceFirst($fieldKey . '.', '', $key);
+                        $permissions[] = compact('label', 'enabled', 'key', 'permission');
+                    }
+
+                    return [ 'permissions' => $permissions, 'label' => $field->getLabel(), 'field' => $fieldSlug, 'key' => $fieldKey ];
+                })->toArray();
+                return $addon;
+            })
+            ->values()
+            ->toArray();
 
 
         app()->platform
